cedb01b298e7190f536d844be585330a9e5ee31a
joel051 <61511114+jojo051@users.noreply.github.com>
2020-07-21 10:24:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+const app = require('./app');
+
+const port = process.env.port || 8000;
+
+app.listen(8000, (err) => {
+  if (err) {
+    throw new Error(`An error occurred: ${err.message}`);
+  }
+  console.log(`Server is listening on ${port}`);
+});
